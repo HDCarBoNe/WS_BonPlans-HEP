2266a1c0a206c21237a59315e2e5d94e42ba1a84
HDCarBoNe <dylan.gogeta@gmail.com>
2018-09-12 15:24:41 +0200

    +Simplification de la lecture du code

--- index.php
+++ index.php
@@ -3,7 +3,10 @@
     include_once('config.php');
     $bdd = new PDO("mysql:host=".Config::SERVERNAME.";dbname=".Config::DBNAME, Config::USER, Config::PASSWORD);
 ?>
-<!-- Ajouter les popups menu -->
+<!-- Ajouter upload des images -->
+<!-- Faire les boutons aceepter/refuser menu admin -->
+<!-- faire notification pour les pro en fonction des besoins -->
+<!-- faire la page article -->
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -29,118 +32,7 @@
 
   <body>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="#">Bon Plans  - HEP</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <!-- Verif login -->
-        <?php if (isset($_POST['login'])){
-                    //  Récupération des données de l'utilisateur grace de son pass crypter
-                    $req = $bdd->prepare("SELECT idUtilisateur, TypeUtilisateur FROM utilisateurs WHERE Identifiant = :login AND mdp = :mdp");
-                    $req->bindParam(":login",$_POST['login']);
-                    $req->bindParam(":mdp",$_POST['mdp']);
-                    $req->execute();
-                    $resultat = $req->fetch();
-                    if (!$resultat)
-                    {
-                        echo '<div class="alert alert-danger" role="alert">Mauvais identifiant ou mot de passe !</div>';
-                    }
-                    else
-                    {
-                        $_SESSION['id'] = $resultat['idUtilisateur'];
-                        echo '<script language="JavaScript" type="text/javascript">window.location.replace("index.php");</script>';
-                    }
-                } ?>
-          <!-- fin verif login -->
-
-       <!-- Faire des conditions pour rajouter des menus dans la barres des taches quand l'utilisateurs est connecter -->
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Accueil
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Bon Plans</a>
-            </li>
-            <?php
-                $req2 = $bdd->prepare("SELECT TypeUtilisateur FROM utilisateurs WHERE idUtilisateur = :id");
-                $req2->bindParam(":id",$_SESSION['id']);
-                $req2->execute();
-                $res = $req2->fetch();
-                $droit = $res['TypeUtilisateur'];
-
-                if (!isset($_SESSION['id'])) {
-                  echo '<li class="nav-item">
-                    <a href="#" class="add-project nav-link" data-toggle="modal" data-target="#connect">Se connecter</a>
-                  </li>';
-                }
-                if ($droit == 2) {
-                  echo '<li class="nav-item">
-                    <a href="#" class="add-project nav-link" data-toggle="modal" data-target="#add_bp">Proposer Bon plan</a>
-                  </li>';
-
-                  echo '<li class="nav-item">
-                    <a href="profil.php?id='.$_SESSION['id'].'" class="nav-link">Profil</a>
-                  </li>';
-
-                  echo '<li class="nav-item">
-                    <a href="deco.php" class="nav-link">Deconnexion</a>
-                  </li>';
-                }
-                if ($droit == 1) {
-                  echo '<li class="nav-item">
-                    <a href="#" class="add-project nav-link" data-toggle="modal" data-target="#add_bp">Proposer Bon plan</a>
-                  </li>';
-
-                  echo '<li class="nav-item dropdown">
-                             <a class="nav-link dropdown-toggle" href="#" id="navadmin" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Administration</a>
-                             <div class="dropdown-menu" aria-labelledby="navadmin">
-                               <a href="#" class="dropdown-item add-project" data-toggle="modal" data-target="#add_news">Ajouter news</a>
-                               <a href="#" class="dropdown-item add-project" data-toggle="modal" data-target="#list_propo">Liste propositions</a>
-                             </div>
-                            </li>';
-
-                  echo '<li class="nav-item">
-                    <a href="profil.php?id='.$_SESSION['id'].'" class="nav-link">Profil</a>
-                  </li>';
-
-                  echo '<li class="nav-item">
-                    <a href="deco.php" class="nav-link">Deconnexion</a>
-                  </li>';
-                }
-                if ($droit == 3) {
-                  echo '<li class="nav-item">
-                    <a href="#" class="add-project nav-link" data-toggle="modal" data-target="#add_bp">Proposer Bon plan</a>
-                  </li>';
-
-                  echo '<li class="nav-item">
-                    <a href="#" class="add-project nav-link" data-toggle="modal" data-target="#add_bp">Proposer son offre </a>
-                  </li>';
-
-                  echo '<li class="nav-item">
-                    <a href="profil.php?id='.$_SESSION['id'].'" class="nav-link">Profil</a>
-                  </li>';
-
-                  echo '<li class="nav-item">
-                    <a href="deco.php" class="nav-link">Deconnexion</a>
-                  </li>';
-                }
-
-            ?>
-            <form class="form-inline">
-              <input class="form-control mr-sm-2" type="search" placeholder="Rechercher" aria-label="Search">
-            </form>
-          </ul>
-        </div>
-
-      </div>
-    </nav>
+    <?php include_once('nav.php'); ?>
 
     <!-- Contneu -->
     <div class="container">
@@ -238,81 +130,5 @@
     <script src="css/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   </body>
-  <!-- popup connection -->
-  <div id="connect" class="modal fade" role="dialog">
-      <div class="modal-dialog">
-          <div class="modal-content">
-              <div class="modal-header login-header">
-                  <h4 class="modal-title">Connecter vous !</h4>
-                  <button type="button" class="close" data-dismiss="modal">×</button>
-              </div>
-              <div class="modal-body">
-                  <form method="POST" action="index.php">
-                  <br>
-                  <label for="login">Pseudo de connection :</label>
-                  <input class="form-control" type="text" name="login" id="login" placeholder="Pseudo" required></input>
-                  <br>
-                  <label for="mdp">Mots de passe :</label>
-                  <input class="form-control" type="password" name="mdp" id="mdp" placeholder="Mots de passe" required></input>
-                  <br>
-                  <input class="btn btn-outline-primary" type="submit" value="se connecter" id="addCr2"/>
-              </div>
-          </div>
-        </form>
-      </div>
-  </div>
-  <!-- fin popup connection -->
-  <!-- popup ajout bon plans -->
-  <div id="add_bp" class="modal fade" role="dialog">
-      <div class="modal-dialog">
-          <div class="modal-content">
-              <div class="modal-header login-header">
-                  <h4 class="modal-title">Partager votre bon plans !</h4>
-                  <button type="button" class="close" data-dismiss="modal">×</button>
-              </div>
-              <div class="modal-body">
-                  <form method="POST" action="add_bp.php">
-                  <br>
-                  <label for="titre">Titre de votre bon plan :</label>
-                  <input class="form-control" type="text" name="titre" id="titre" placeholder="Titre" required></input>
-                  <br>
-                  <label for="text-area">Expliquer nous votre bon plan :</label>
-                  <textarea class="form-control" name="text-area" id="text-area" rows="3" placeholder="Le lieu, les conditions du bon plan, les dates jusqu'au quel le bon plan sera disponible ... " required></textarea>
-                  <br>
-                  <input type="hidden" value="<?php echo $_SESSION['id'] ?>" name="id_uti"></input>
-                  <input class="btn btn-outline-primary" type="submit" value="Partager !" id="addCr2"/>
-              </div>
-          </div>
-        </form>
-      </div>
-  </div>
-  <!-- fin popup ajout bon plans -->
-  <!-- popup ajout news-->
-  <div id="add_news" class="modal fade" role="dialog">
-      <div class="modal-dialog">
-          <div class="modal-content">
-              <div class="modal-header login-header">
-                  <h4 class="modal-title">Partager une News</h4>
-                  <button type="button" class="close" data-dismiss="modal">×</button>
-              </div>
-              <div class="modal-body">
-                  <form method="POST" action="add_news.php">
-                  <br>
-                  <label for="titre">Titre de votre news :</label>
-                  <input class="form-control" type="text" name="titre" id="titre" placeholder="Titre" required></input>
-                  <br>
-                  <label for="text-area">Détails de la news :</label>
-                  <textarea class="form-control" name="text-area" id="text-area" rows="3" placeholder="Le lieu, les conditions de la news, les dates concernant la news ... " required></textarea>
-                  <br>
-                  <label for="photo"> Ajouter une image</label>
-                  <input type="file" class="form-control-file" id="photo" name="photo"></input>
-                  <br>
-                  <input type="hidden" value="<?php echo $_SESSION['id'] ?>" name="id_uti"></input>
-                  <input class="btn btn-outline-primary" type="submit" value="Partager !" id="addCr2"/>
-              </div>
-          </div>
-        </form>
-      </div>
-  </div>
-  <!-- fin popup ajout bon plans -->
+  <?php include_once('popup.php'); ?>
 </html>
